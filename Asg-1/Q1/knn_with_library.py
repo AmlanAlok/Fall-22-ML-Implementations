b46ddc6f703b190a62b8b813fd9fb0713f75734d
Amlan Alok <amlanalok@gmail.com>
2022-09-20 00:04:01 -0500

    cleaned code

--- Asg-1/Q1/knn_with_library.py
+++ Asg-1/Q1/knn_with_library.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
 
 
 def clean_data(line):
@@ -16,7 +15,6 @@ def fetch_data(filename):
         input_data = f.readlines()
         clean_input = list(map(clean_data, input_data))
         f.close()
-
     return clean_input
 
 
@@ -27,31 +25,14 @@ def read_data(filename, col_headers):
     input_data = fetch_data(file_path)
     # csv_filename = filename.replace('.txt', '.csv')
     # np.savetxt(csv_filename, np.array(input_data), delimiter=',', fmt='%s', comments='', header=names)
-    # print(input_data)
     df = pd.DataFrame(input_data, columns = col_headers)
     return df
 
-def input_test_data():
-    names = 'height,weight,age'
-    file_path = '../dataset/1a-test.txt'
-    test_data = fetch_data(file_path)
-    np.savetxt('1a-test.csv', np.array(test_data), delimiter=',', fmt='%s', comments='', header=names)
-    # print(test_data)
 
-def knn_implementation(dataset, test_dataset, k, metric):
+def knn_implementation(train_dataset, test_dataset, k, metric):
 
-    # dataset = pd.read_csv(training_data)
-    X_train = dataset.iloc[:, :-1].values
-    y_train = dataset.iloc[:, -1].values
-
-    # le = LabelEncoder()
-    # label = le.fit_transform(y_train)
-
-    '''Normalize features so all of them can be uniformly evaluated'''
-    # scaler = StandardScaler()
-    # scaler.fit(X_train)
-    # X_train = scaler.transform(X_train)
-    # print(X_train)
+    X_train = train_dataset.iloc[:, :-1].values
+    y_train = train_dataset.iloc[:, -1].values
 
     '''Normalize features so all of them can be uniformly evaluated'''
     min_max_scaler = MinMaxScaler()
@@ -70,10 +51,8 @@ def knn_implementation(dataset, test_dataset, k, metric):
     classifier.fit(X_train, y_train)
 
     '''Reading test data'''
-    # test_dataset = pd.read_csv(test_data)
     X_test = test_dataset.iloc[:, :].values
     X_test = min_max_scaler.fit_transform(X_test)
-    # print(X_test)
 
     y_pred = classifier.predict(X_test)
     return y_pred
