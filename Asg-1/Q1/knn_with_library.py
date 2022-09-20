073bae6f1a884eada19541d1d7196e53ad5c4b4b
Amlan Alok <amlanalok@gmail.com>
2022-09-20 00:05:01 -0500

    cleaned code

--- Asg-1/Q1/knn_with_library.py
+++ Asg-1/Q1/knn_with_library.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
