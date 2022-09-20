9ce349358c32a4bdbd1f39048d93ecf57fb4d286
Amlan Alok <amlanalok@gmail.com>
2022-09-20 03:20:06 -0500

    test cases added

--- Asg-1/Q1/Q1_test.py
+++ Asg-1/Q1/Q1_test.py
@@ -1,6 +1,7 @@
 import unittest
 from knn_with_library import library_output
-from x2 import scratch_code_output
+from knn_without_library import scratch_code_output
+import numpy as np
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,6 +13,7 @@ class MyTestCase(unittest.TestCase):
             print(library_output(k, 'manhattan'))
             print(library_output(k, 'minkowski'))
             print('-----------------')
+
         print('-----')
         for k in [1, 3, 7]:
             print(scratch_code_output(k, 'euclidean'))
@@ -19,11 +21,27 @@ class MyTestCase(unittest.TestCase):
             print(scratch_code_output(k, 'minkowski'))
             print('-----------------')
 
+    def test_expected_output(self):
+        print('')
+        print(library_output(1, 'euclidean'))
+        print(library_output(3, 'euclidean'))
+        print(library_output(7, 'euclidean'))
+
+        print(library_output(1, 'manhattan'))
+        print(library_output(3, 'manhattan'))
+        print(library_output(7, 'manhattan'))
+
+        print(library_output(1, 'minkowski'))
+        print(library_output(3, 'minkowski'))
+        print(library_output(7, 'minkowski'))
+
     def test_x1(self):
         for k in [1, 5, 7]:
             for metric in ['manhattan', 'euclidean', 'minkowski']:
                 self.assertEqual((library_output(k, metric) == scratch_code_output(k, metric)).all(), True)
 
+    def test_for_students(self):
+        self.assertEqual(True, (scratch_code_output(1, 'euclidean') == np.array(['W', 'M', 'M', 'W'])).all())
 
 if __name__ == '__main__':
     unittest.main()
