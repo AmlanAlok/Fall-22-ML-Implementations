01672275d99866b7d5355da66ba1663444efc0f8
Amlan Alok <amlanalok@gmail.com>
2022-09-20 00:01:12 -0500

    sklearn knn implemented

--- Asg-1/Q1/Q1_test.py
+++ Asg-1/Q1/Q1_test.py
@@ -1,15 +1,16 @@
 import unittest
-from knn_with_lib import lib_output
+from knn_with_library import lib_output
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
+    def test_knn_predictions_via_sklearn(self):
         print('')
-        print(lib_output(1))
-        # print(lib_output(3))
-        # print(lib_output(5))
-        # print(lib_output(7))
+        for k in [1,3,7]:
+            print(lib_output(k, 'euclidean'))
+            print(lib_output(k, 'manhattan'))
+            print(lib_output(k, 'minkowski'))
+            print('-----------------')
 
 
 if __name__ == '__main__':
--- Asg-1/Q1/knn_with_library.py
+++ Asg-1/Q1/knn_with_library.py
@@ -11,7 +11,6 @@ def clean_data(line):
 
 
 def fetch_data(filename):
-    # print('inside func '+ inspect.stack()[0][3])
 
     with open(filename, 'r') as f:
         input_data = f.readlines()
@@ -39,41 +38,7 @@ def input_test_data():
     np.savetxt('1a-test.csv', np.array(test_data), delimiter=',', fmt='%s', comments='', header=names)
     # print(test_data)
 
-
-# def knn_implementation(training_data, test_data, k):
-#
-#     dataset = pd.read_csv(training_data)
-#     X_train = dataset.iloc[:, :-1].values
-#     y_train = dataset.iloc[:, -1].values
-#
-#     # le = LabelEncoder()
-#     # label = le.fit_transform(y_train)
-#
-#     '''Normalize features so all of them can be uniformly evaluated'''
-#     # scaler = StandardScaler()
-#     # scaler.fit(X_train)
-#     # X_train = scaler.transform(X_train)
-#     # print(X_train)
-#
-#     '''Normalize features so all of them can be uniformly evaluated'''
-#     min_max_scaler = MinMaxScaler()
-#     X_train = min_max_scaler.fit_transform(X_train)
-#
-#     '''KNN Classifier'''
-#     classifier = KNeighborsClassifier(n_neighbors=k)
-#     classifier.fit(X_train, y_train)
-#
-#     '''Reading test data'''
-#     test_dataset = pd.read_csv(test_data)
-#     X_test = test_dataset.iloc[:, :].values
-#     X_test =  min_max_scaler.fit_transform(X_test)
-#     # print(X_test)
-#
-#     y_pred = classifier.predict(X_test)
-#     pass
-#     return y_pred
-
-def knn_implementation(dataset, test_dataset, k):
+def knn_implementation(dataset, test_dataset, k, metric):
 
     # dataset = pd.read_csv(training_data)
     X_train = dataset.iloc[:, :-1].values
@@ -92,33 +57,37 @@ def knn_implementation(dataset, test_dataset, k):
     min_max_scaler = MinMaxScaler()
     X_train = min_max_scaler.fit_transform(X_train)
 
+    p = 0
+    if metric == 'manhattan':
+        p = 1
+    elif metric == 'euclidean':
+        p = 2
+    elif metric == 'minkowski':
+        p = 3
+
     '''KNN Classifier'''
-    classifier = KNeighborsClassifier(n_neighbors=k)
+    classifier = KNeighborsClassifier(n_neighbors=k, p=p)
     classifier.fit(X_train, y_train)
 
     '''Reading test data'''
     # test_dataset = pd.read_csv(test_data)
     X_test = test_dataset.iloc[:, :].values
-    X_test =  min_max_scaler.fit_transform(X_test)
+    X_test = min_max_scaler.fit_transform(X_test)
     # print(X_test)
 
     y_pred = classifier.predict(X_test)
-    pass
     return y_pred
 
 
-def lib_output(k):
+def lib_output(k, metric):
     train_filename = '1a-training.txt'
     test_filename = '1a-test.txt'
-    # training_data_header = 'height,weight,age,label'
-    # test_data_header = 'height,weight,age'
+
     training_col_header = ['height', 'weight', 'age', 'col']
     test_col_header = ['height', 'weight', 'age']
-    # train_csv = read_data(train_filename, training_data_header)
-    # test_csv = read_data(test_filename, test_data_header)
+
     train_df = read_data(train_filename, training_col_header)
     test_df = read_data(test_filename, test_col_header)
 
-    # predictions = knn_implementation(train_csv, test_csv, k)
-    predictions = knn_implementation(train_df, test_df, k)
+    predictions = knn_implementation(train_df, test_df, k, metric)
     return predictions
