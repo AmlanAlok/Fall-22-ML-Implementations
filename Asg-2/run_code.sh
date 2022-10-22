6bb1848918f9289392e34342b4152d433b6d7ab0
Amlan Alok <amlanalok@gmail.com>
2022-10-22 16:34:54 -0500

    script to run all files

--- Asg-2/run_code.sh
+++ Asg-2/run_code.sh
@@ -1,2 +1,13 @@
 
 python3 Q1/Q1_A.py
+python3 Q1/Q1_B.py
+python3 Q1/Q1_C.py
+python3 Q1/Q1_D.py
+
+python3 Q2/Q2_B.py
+python3 Q2/Q2_C.py
+python3 Q2/Q2_D.py
+
+python3 Q3/Q3_AB.py
+python3 Q3/Q3_C.py
+python3 Q3/Q3_D.py
