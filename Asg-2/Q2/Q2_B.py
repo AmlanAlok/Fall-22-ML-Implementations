f1c85e5d67a97aa1014608a520a9552be539fd9e
Amlan Alok <amlanalok@gmail.com>
2022-11-01 14:39:54 -0500

    made plot scatter

--- Asg-2/Q2/Q2_B.py
+++ Asg-2/Q2/Q2_B.py
@@ -9,22 +9,32 @@ def Q2_B_sol(size):
     filename = 'datasets/Q1_B_train.txt'        # command
     input_data = fetch_data(filename)
     x_data, y_data = separate_input_output_limit(input_data, size)
-
-    x_array = np.linspace(-3, 3, 1000)
-
     y_prediction, y_array = [], []
 
-    for x in x_array:
+    ''' For a smooth line'''
+    x_array = np.linspace(-3, 3, 1000)
+
+    # for x in x_array:
+    #     prediction = train_and_prediction(x, gamma, 128, x_data, y_data)
+    #     y_prediction.append(prediction)
+
+    for x_point in x_data:
+        x = x_point[0]
         prediction = train_and_prediction(x, gamma, 128, x_data, y_data)
         y_prediction.append(prediction)
 
     y_prediction = np.array(y_prediction)
 
-    plt.plot(x_array, y_prediction)
+    # plt.plot(x_array, y_prediction)
+    # plt.plot(x_data, y_data)
+
+    # plt.scatter(x_data, y_data)           # to see plot of given data
+    plt.scatter(x_data, y_prediction)       # gives good output
+
     plt.title('Locally Weighted Linear Regression')
     # plt.legend(line_names)
-    # plt.savefig('Q2_B_Pic')
-    plt.savefig('Q2/Q2_B_Pic')
+    # plt.savefig('Q2_B_Pic_training_points')
+    plt.savefig('Q2_B_Pic')
 
 
 def main():
